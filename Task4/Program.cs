f498c4a8d2c8467222d9dc6dfd4e37977f38aa22
Gleb <gleb.gadbrat@mail.ru>
2022-03-12 16:55:55 +0300

    Задание 4 - Написать программу вычисления произведения чисел от 1 до N

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите конечное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int proizv = 1;
+for (int i = 1; i <= N; i++)
+{
+    proizv *= i;
+}
+Console.Write("Произведение чисел равно ");
+Console.WriteLine(proizv);
