0568c1c5114162d59e12afe125a50be91c86e05f
Gleb <gleb.gadbrat@mail.ru>
2022-03-12 16:46:42 +0300

    Задание 3 - Подсчитать сумму цифр в числе

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum = 0;
+for (; N > 0; N /= 10)
+{
+    Sum += N%10;
+}
+Console.Write("Сумма цифр числа равна ");
+Console.WriteLine(Sum);
