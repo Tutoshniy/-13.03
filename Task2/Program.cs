c64bd6f8879bb463cdb61f7dbe7d4757c1c10696
Gleb <gleb.gadbrat@mail.ru>
2022-03-12 16:30:08 +0300

    Задание 2 - Найти сумму чисел от 1 до А

---
+++ Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите конечное число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int Sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    Sum += i;
+}
+Console.Write("Сумма цифр равна: ");
+Console.WriteLine(Sum);
